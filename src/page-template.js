c63275b67d313f1faf9e1f7f90b102840e183ac7
Naomi-Eckhoff <nsuibhne@gmail.com>
2021-11-12 15:28:36 -0600

    Completed program

--- src/page-template.js
+++ src/page-template.js
@@ -19,9 +19,9 @@ const generateProjects = projectsArr => {
       <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
       <div class="flex-row justify-space-between">
       ${projectsArr
-      .filter(({ feature }) => feature)
-      .map(({ name, description, languages, link }) => {
-        return `
+        .filter(({ feature }) => feature)
+        .map(({ name, description, languages, link }) => {
+          return `
           <div class="col-12 mb-2 bg-dark text-light p-3">
             <h3 class="portfolio-item-title text-light">${name}</h3>
             <h5 class="portfolio-languages">
@@ -32,13 +32,14 @@ const generateProjects = projectsArr => {
             <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
           </div>
         `;
-      })
-      .join('')}
+        })
+        .join('')}
 
       ${projectsArr
-      .filter(({ feature }) => !feature)
-      .map(({ name, description, languages, link }) => {
-        return `
+        .filter(({ feature }) => !feature)
+        .map(({ name, description, languages, link }) => {
+          console.log(languages);
+          return `
           <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
             <h3 class="portfolio-item-title text-light">${name}</h3>
             <h5 class="portfolio-languages">
@@ -49,8 +50,9 @@ const generateProjects = projectsArr => {
             <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
           </div>
         `;
-      })
-      .join('')}
+        })
+        .join('')}
+    
       </div>
     </section>
   `;
@@ -64,6 +66,7 @@ module.exports = templateData => {
   return `
   <!DOCTYPE html>
   <html lang="en">
+  
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -79,8 +82,7 @@ module.exports = templateData => {
       <div class="container flex-row justify-space-between align-center py-3">
         <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
         <nav class="flex-row">
-          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github
-    }">GitHub</a>
+          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">GitHub</a>
         </nav>
       </div>
     </header>
@@ -89,7 +91,7 @@ module.exports = templateData => {
       ${generateProjects(projects)}
     </main>
     <footer class="container text-center py-3">
-      <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+      <h3 class="text-dark">&copy;2020 by ${header.name}</h3>
     </footer>
   </body>
   </html>
